1a51b215bbf1ec1ec7b5cd32d1455c2c8bb35637
piyushkrsingh07 <piyush23153101@akgec.ac.in>
2024-11-01 15:40:24 +0530

    connectdemo

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import Sidebar from "./components/Sidebar"
+import Attendance from './components/Attendance'
+import Timetable from './components/Timetable'
+import Profile from './components/Profile'
+
+import {Route,Routes} from "react-router-dom"
+
+function App() {
+
+  const[IsLoggedIn,setIsLoggedIn]=useState(false);
+
+
+  return (
+    <>
+    <div>
+      <Navbar IsLoggedIn={IsLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+
+      <Routes>
+
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
+        <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn}/>} />
+        <Route path="/dashboard" element={<Dashboard setIsLoggedIn={setIsLoggedIn}/>} />
+
+              <Route path="/Attendance" element={<Attendance setIsLoggedIn={setIsLoggedIn}/>} />
+              <Route path="/Timetable" element={<Timetable setIsLoggedIn={setIsLoggedIn}/>} />
+              <Route path="/Profile" element={<Profile setIsLoggedIn={setIsLoggedIn}/>} />
+
+
+
+      </Routes>
+
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Attendance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Attendance = () => {
+  return (
+    <div>
+      <h2 className="text-xl">Attendance Page</h2>
+    </div>
+  )
+}
+
+export default Attendance
